f2782fcba4dbec6bdcd77bf17ade12b9beea560e
Eliseu Videira <eliseuvideira1@gmail.com>
2019-12-15 12:58:41 -0300

    Added buttons and sounds

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audio_cache.dart';
 
 void main() => runApp(MyApp());
 
@@ -20,8 +21,35 @@ class MyApp extends StatelessWidget {
 }
 
 class MainPage extends StatelessWidget {
+  final player = AudioCache();
+
+  Widget _buildButton(Color color, int number) {
+    return Expanded(
+      child: FlatButton(
+        color: color,
+        child: null,
+        onPressed: () {
+          player.play('note$number.wav');
+        },
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          _buildButton(Colors.red, 1),
+          _buildButton(Colors.orange, 2),
+          _buildButton(Colors.yellow, 3),
+          _buildButton(Colors.green, 4),
+          _buildButton(Colors.teal, 5),
+          _buildButton(Colors.blue, 6),
+          _buildButton(Colors.purple, 7),
+        ],
+      ),
+    );
   }
 }
